ec8dfb7fa291e63423e2cd4eabe440ffabad9209
ArT <74541549+AliRezaT96@users.noreply.github.com>
2022-10-23 09:44:24 +0330

    inference function for converter

--- convert.py
+++ convert.py
@@ -1,1 +1,13 @@
+from model import converter
 
+def convert(currency):
+  while(True):
+    inp = input('Enter text:')
+    print(converter(inp,currency))
+
+if __name__=='__main__':
+
+    args_parser = argparse.ArgumentParser()
+    args_parser.add_argument('--currency', dest='currency', required=True)
+    args = args_parser.parse_args()
+    convert(currency=args.currency)
