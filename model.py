a92a024aa03114519148815f00004956b3da8502
ArT <74541549+AliRezaT96@users.noreply.github.com>
2022-10-23 09:51:15 +0330

    model templates

--- model.py
+++ model.py
@@ -1,1 +1,19 @@
 
+
+def converter(text,currency):
+  """
+    text: a text that hopefully contains some amount of money 
+    currency: the currency we want to have. for now [rial or toman]
+    
+    returns the money in desired currency
+  """
+  
+  out = ''
+  return out
+
+def RialToToman():
+  pass
+
+def TomanToRial():
+  pass
+  
